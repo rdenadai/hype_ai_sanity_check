a4a65557b337bd58d4a9d95f0f78059535442c9f
Rodolfo De Nadai <rdenadai@gmail.com>
2024-07-12 00:38:47 -0300

    chore: Mess up name again

--- server.sh
+++ server.sh
@@ -2,6 +2,6 @@
 
 docker compose down && docker compose up -d
 
-docker compose -f docker-compose.yml run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d www.aihypesafetycheck.tech
+docker compose -f docker-compose.yml run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d www.aihypesanitycheck.tech
 
 docker compose down certbot
